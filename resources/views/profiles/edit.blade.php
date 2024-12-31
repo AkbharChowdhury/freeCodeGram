50fcd7e5246a5685e54c20c05dfb421afde35a6f
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2024-12-31 11:04:03 +0000

    x

--- resources/views/profiles/edit.blade.php
+++ resources/views/profiles/edit.blade.php
@@ -10,14 +10,29 @@
                 <h1>Edit Profile</h1>
                 <hr>
             </div>
-            <form  class="row g-3" action="{{ route('posts.store') }}" method="post" novalidate enctype="multipart/form-data">
+            <form  class="row g-3" action="{{ route('profile.update', $user->id)  }}" method="post" novalidate enctype="multipart/form-data">
                 @csrf
+                @method('PATCH')
                 <div class="col-md-8">
-                    <label for="caption" class="form-label">Post Caption</label>
-                    <input type="text" class="form-control @error('caption') is-invalid @enderror" id="caption" value="{{ old('caption') }}"
-                           name="caption" autofocus>
-                   @include('includes.errors', ['key'=> ''])
+                    <label for="title" class="form-label">title</label>
+                    <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" value="{{ old('title') ?? $user->profile->title}}"
+                           name="title" autofocus>
+                   @include('includes.errors', ['key'=> 'title'])
                 </div>
+
+
+
+                <div class="col-md-8">
+                    <label for="description" class="form-label">description</label>
+                    <textarea class="form-control form-control @error('description') is-invalid @enderror" name="description" id="description" rows="3">{{ old('description') ?? $user->profile->title }}</textarea>
+                    @include('includes.errors', ['key'=> 'description'])
+                </div>
+
+
+
+
+
+
 
                 <div class="row mt-3">
 
@@ -38,7 +53,7 @@
 
 
                 <div class="col-12">
-                    <button type="submit" class="btn btn-success">Add Post</button>
+                    <button type="submit" class="btn btn-success">Update Profile</button>
                 </div>
 
         </div>
