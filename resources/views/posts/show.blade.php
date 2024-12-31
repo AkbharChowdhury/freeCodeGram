83c5b5b3e42a614059d16158259fd2ae5b0e044d
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2024-12-31 17:11:48 +0000

    x

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,16 +4,33 @@
         @include('includes.breadcrumb', ['homeLink' =>  route('profile.show', $post->user->id), 'title' =>  $post->user->username.' - '. $post->caption])
 
         <div class="row">
-           <div class="col-8">
-               <img src="/storage/{{ $post->image }}" alt="{{ $post->caption }}" class="w-100">
-           </div>
-           <div class="col-4">
-               <div>
-                   <h3>{{ $post->user->username }}</h3>
-                   <p>{{ $post->caption }}</p>
-               </div>
-           </div>
-       </div>
+            <div class="col-8">
+                <img src="/storage/{{ $post->image }}" alt="{{ $post->caption }}" class="w-100">
+            </div>
+            <div class="col-4">
+                <div>
+                    <div class="d-flex align-items-center">
+                        <div class="pr-3">
+                            <img src="/storage/{{ $post->user->profile->image }}"
+                                 alt="{{ $post->user->username }}'s profile image" class="w-100 rounded-circle"
+                                 style="max-width: 40px">
+                        </div>
+                        <div>
+                            <div class="fw-bold">
+                                <a href="{{  route('profile.show', $post->user->id) }}">{{ $post->user->username }}</a> | 
+                                <a href="" class="pl-3">follow</a>
+                            </div>
+                        </div>
+                    </div>
+                    <hr>
+
+
+                    <div>
+                        <span class="fw-bold">{{ $post->user->username }}</span> <br>{{ $post->caption }}
+                    </div>
+                </div>
+            </div>
+        </div>
 
     </div>
 @endsection
