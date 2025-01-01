479a6a62e22330f846b0be89cbc769390b0a4313
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2025-01-01 11:16:19 +0000

    d

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,11 +3,13 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Mail\NewUserWelcomeMail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Mail;
 
 class User extends Authenticatable
 {
@@ -65,6 +67,7 @@ class User extends Authenticatable
             $user->profile()->create([
                 'title'=> $user->username
             ]);
+            Mail::to($user->email)->send(new  NewUserWelcomeMail());
         });
     }
 
---
+++ resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,12 @@
+<x-mail::message>
+# Welcome to {{ config('app.name') }}
+
+This is a community of fellow developers and we love that you joined us
+
+{{--<x-mail::button :url="''">--}}
+{{--Button Text--}}
+{{--</x-mail::button>--}}
+
+Thanks,<br>
+{{ config('app.name') }}
+</x-mail::message>
